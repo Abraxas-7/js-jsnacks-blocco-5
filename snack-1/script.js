0cea09ab3b99dfe45e2e5ab4e618f5611b79e39f
Abraxas-7 <arber.beshaj.02@gmail.com>
2024-11-15 13:55:14 +0100

    es1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,7 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+const result = names.join(', ');
+
+console.log(result);
